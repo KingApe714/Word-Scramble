1a233db7860772bf595fe7a6edade0ce2646ec01
jonathandiaz <jonathan.diaz714@gmail.com>
2021-05-22 22:14:12 -0400

    grabbing info from dictionary

---
+++ script.js
@@ -0,0 +1,69 @@
+const modalBtn = document.querySelector('.modal-button');
+const modalBg = document.querySelector('.modal-bg');
+const modalClose = document.querySelector('.modal-close');
+const instructions = document.querySelector('.instructions')
+const text = document.querySelector('.instructions');
+const strText = text.textContent;
+const splitText = strText.split(' ');
+text.textContent = "";
+
+for (let i = 0; i < splitText.length; i++) {
+    text.innerHTML += "<span class='letter'>" + splitText[i] + "</span>";
+    text.innerHTML += "&nbsp;"
+}
+
+modalBtn.addEventListener('click', function() {
+    modalBg.classList.add('bg-active');
+    
+    let char = 0;
+    let timer = setInterval(onTick, 50);
+    
+    function onTick() {
+        const span = text.querySelectorAll('.letter')[char];
+        if (span) span.classList.add('fade');
+        char++;
+        if (char === splitText.length) {
+            complete();
+            return;
+        }
+    }
+    
+    function complete() {
+        clearInterval(timer);
+        timer = null;
+    }
+    
+})
+modalClose.addEventListener('click', function() {
+    modalBg.classList.remove('bg-active');
+})
+
+let test;
+
+function grabData(data) {
+    // console.log(data)
+    return data
+}
+
+function loadText() {
+    let foo;
+    fetch('dictionary.txt')
+        .then(res => {
+            // console.log(res)
+            // console.log(res.text)
+            return res.text();
+        })
+        .then(data => {
+            grabData(data)
+            // test = data;
+            return data;
+        })
+    console.log(foo)
+    // console.log(bar)
+    return foo
+}
+
+
+// console.log(test)
+
+console.log(loadText());
