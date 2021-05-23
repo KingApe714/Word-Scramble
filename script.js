a33aa28271e56ef28875a44887a221bf78192de2
jonathandiaz <jonathan.diaz714@gmail.com>
2021-05-23 02:53:33 -0400

    testing read file

--- script.js
+++ script.js
@@ -38,32 +38,90 @@ modalClose.addEventListener('click', function() {
     modalBg.classList.remove('bg-active');
 })
 
-let test;
+// const fetchDictionary = fetch('dictionary.txt')
+//     .then(res => {
+//         // console.log(res)
+//         return res.text();
+//     })
+//     .then(data => {
+//         // console.log(data);
+//         return data;
+//     })
+const test = async () => {
+    const a = await fetchDictionary;
+    // const b = await a;
+    // console.log(a)
+    // console.log(a)
+    return a
+}
 
-function grabData(data) {
+// const fetchWords = () => (
+//     fetch('dictionary.txt')
+// )
+
+// const receiveDictionary = (dictionary) => ({
+//     dictionary
+// })
+
+// const fetchDictionary = () => dispatch => {
+    
+//     return fetchWords()
+//         .then(words => dispatch(receiveDictionary(words)))
+// }
+
+// let test1 = fetchDictionary()
+
+let stuff, k;
+// console.log(test1())
+async function otherTest() {
+    const a = await fetch('dictionary.txt')
+    const data = await a.text();
     // console.log(data)
+
+    stuff = data;
+
+    let event = new Event("test", { bubbles: true });
+    let k = document.dispatchEvent(event);
+    console.log(k)
     return data
 }
+// console.log(otherTest())
+otherTest();
 
-function loadText() {
-    let foo;
-    fetch('dictionary.txt')
-        .then(res => {
-            // console.log(res)
-            // console.log(res.text)
-            return res.text();
-        })
-        .then(data => {
-            grabData(data)
-            // test = data;
-            return data;
-        })
-    console.log(foo)
-    // console.log(bar)
-    return foo
+let flag = false;
+let tester;
+
+addEventListener("test", function(){
+    flag = true;
+    // console.log(fetch('dictionary.txt'))
+    // alert("pishglang")
+    // return (fetch('dictionary.txt'))
+    // console.log(tester)
+})
+
+setTimeout( ()=> 
+{
+    if( flag ){
+        console.log(stuff); 
+    }
+},0);
+
+
+
+console.log(stuff)
+
+// let event = new Event("test", {bubbles: true})
+
+// let k = document.dispatchEvent(event)
+
+
+console.log(k)
+
+function one(){
+
+}
+function two(){
+
+
 }
 
-
-// console.log(test)
-
-console.log(loadText());
