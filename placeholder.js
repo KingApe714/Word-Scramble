782daf7551602fe83b6081b3406e38d1ef3ba6bc
jonathandiaz <jonathan.diaz714@gmail.com>
2021-05-31 00:22:59 -0400

    displaying dashes

--- placeholder.js
+++ placeholder.js
@@ -15,9 +15,13 @@ function placeHolders() {
 }
 
 function holders(arr) {
+    let dashes = ''
+    for (j = 0; j < arr[0].length; j++) {
+        dashes += '_'
+    }
     const div = document.createElement("div")
     for (let i = 0; i < arr.length; i++) {
-        div.innerHTML += `<div>${arr[i]}</div>`
+        div.innerHTML += `<div>${dashes}</div>`
     }
     return div
 }
